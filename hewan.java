c461bc5c205c68d4aebaeb5d9c8e9abe9287b211
Irfan21 <nurirfan21@gmail.com>
2022-09-17 16:26:54 +0700

    update

---
+++ hewan.java
@@ -0,0 +1,17 @@
+public class hewan {
+    String mata, mulut, telinga, ekor;
+    int kaki;
+    
+    String bermain(String m){
+        return m;
+    }
+    
+    String makan (String k){
+        return k;
+    }
+    
+    Boolean tidur (Boolean t){
+        return t;
+    }
+}
+
